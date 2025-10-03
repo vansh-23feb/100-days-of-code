e5c3ef900213f52e64479d31c7f38902c9457694
vansh-23feb <vanshjaiswal23022007@gmail.com>
2025-10-03 15:02:30 +0530

    done first que

---
+++ OneDrive/Desktop/100-days-of-code/q1.c
@@ -0,0 +1,15 @@
+//Write a program to input two numbers and display their sum.
+# include<stdio.h>
+# include<conio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int m;
+    scanf("%d",&m);
+    int c;
+    c=n+m;
+    printf("%d",c);
+    getch();
+    return 0;
+}
