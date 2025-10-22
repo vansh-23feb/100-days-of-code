2c04541b4e89cda38aef67c271bd197adaca92be
vansh-23feb <vanshjaiswal23022007@gmail.com>
2025-10-22 09:30:53 +0530

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,9 @@
+//Write a program to input two numbers and display their sum.
+# include<stdio.h>
+int main()
+{
+    int n1,n2;
+    scanf("%d %d",&n1,&n2);
+    printf("%d",n1+n2);
+    return 0;
+}
